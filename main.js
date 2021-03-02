2a63d4c3d72a0882f5027ae69753196bad1986cb
Tanmay C <66797097+TanmayC-Web@users.noreply.github.com>
2021-03-02 18:01:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+song = "";
+
+function preload () {
+    song = loadSound("music.mp3");
+}
+
+function setup () {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video.createCapture(VIDEO);
+    video.hide();
+}
+
+function draw () {
+    image(video, 0, 0, 600, 500);
+    fill("#FF0000");
+    strok("#FF0000");
+}
+
+function play () 
+{
+    song.play();
+}
